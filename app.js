a43c7eea9f97bccc9dbec54e2568003b4a72a1da
Guillermo <gbarcomu@gmail.com>
2018-10-08 02:16:21 +0200

    Transaction solved

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const bodyParser = require('body-parser');
 const http = require('http');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /****** ETHEREUM ******/
 const ethConfig = require('./ethConfig.js');
 const ethConnectionHelper = require('./ethConnectionHelper.js');
 const web3 = ethConnectionHelper.getConnection();
-const myAccount = web3.eth.accounts.privateKeyToAccount(ethConfig.ETHEREUM_ACCOUNT)
+const myAccount = web3.eth.accounts.privateKeyToAccount(ethConfig.ETHEREUM_ACCOUNT);
+var Tx = require('ethereumjs-tx');
 
 app.get(ethConfig.API_PATH, (req, res) => res.send("App is Connected and Running!"));
 
@@ -21,18 +22,40 @@ app.listen(ethConfig.PORT, () => console.log('App listening on port ' + ethConfi
 app.get(ethConfig.API_PATH + '/balance', (req, res) => {
   web3.eth.getBalance(myAccount.address).then(response => {
     let balance = web3.utils.fromWei(response, 'ether');
-    res.send({balance: balance});
+    res.send({ balance: balance });
   });
 });
 
 app.post(ethConfig.API_PATH + '/funds', (req, res) => {
   const { recipientAccount, amount } = req.body;
-  console.log(recipientAccount + " " + amount)
-  res.send();
+  web3.eth.getGasPrice()
+    .then(gasPrice => {
+      web3.eth.getTransactionCount(myAccount.address)
+        .then(nofTransactions => {
+          let rawTx = {
+            nonce: web3.utils.toHex(nofTransactions),
+            gas: web3.utils.toHex('21000'),
+            gasPrice: web3.utils.toHex(gasPrice),
+            to: recipientAccount,
+            value: web3.utils.toHex(amount),
+          }
+
+          let tx = new Tx(rawTx);
+          tx.sign(Buffer.from(ethConfig.ETHEREUM_PK, 'hex'));
+          let serializedTx = tx.serialize();
+
+          web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+            .on('receipt', response => {
+              res.send(response);
+            });
+        })
+    });
 });
 
 app.get(ethConfig.API_PATH + '/transaction', (req, res) => {
   const { transactionhash } = req.query;
-  console.log(transactionhash);
-  res.send();
+  web3.eth.getTransaction(transactionhash)
+    .then(response => {
+      res.send(response);
+    });
 });
